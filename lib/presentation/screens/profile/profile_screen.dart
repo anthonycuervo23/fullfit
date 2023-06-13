c6dbec76adb6dcc4ba220e54b240d0f36fe48324
Jean Cuervo <jean.cuervo@datagroup.de>
2023-06-13 17:57:40 +0200

    actualizado pods de ios

--- lib/presentation/screens/profile/profile_screen.dart
+++ lib/presentation/screens/profile/profile_screen.dart
@@ -1,22 +1,161 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fullfit_app/presentation/providers/providers.dart';
 
 class ProfileScreen extends ConsumerWidget {
   const ProfileScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authNotifier = ref.watch(authProvider.notifier);
-
+    var isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
+    final colors = Theme.of(context).colorScheme;
+    final textStyles = Theme.of(context).textTheme;
+    final person = ref.watch(personProvider).person;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ProfileScreen'),
+        // leading: IconButton(onPressed: () {}, icon: const Icon(LineAwesomeIcons.angle_left)),
+        title: const Text('Profile'),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(isDark ? Icons.sunny : Icons.night_shelter))
+        ],
       ),
-      body: Center(
-        child: FilledButton.tonal(
-            onPressed: () => authNotifier.logout(),
-            child: const Text('LogOut')),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              /// -- IMAGE
+              Stack(
+                children: [
+                  SizedBox(
+                      width: 120.w,
+                      height: 120.h,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child:
+                              Image(image: NetworkImage(person!.profilePic)))),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      width: 35,
+                      height: 35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: colors.primary),
+                      child: const Icon(
+                        Icons.edit,
+                        color: Colors.black,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Text(person.fullName, style: textStyles.titleMedium),
+              Text(person.email, style: textStyles.bodyMedium),
+              const SizedBox(height: 20),
+
+              /// -- BUTTON
+              SizedBox(
+                width: 200.w,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: colors.primary,
+                      side: BorderSide.none,
+                      shape: const StadiumBorder()),
+                  child: const Text('Edit Profile'),
+                ),
+              ),
+              const SizedBox(height: 30),
+              const Divider(),
+              const SizedBox(height: 10),
+
+              /// -- MENU
+              ProfileMenuWidget(
+                  title: "Settings", icon: Icons.offline_bolt, onPress: () {}),
+              ProfileMenuWidget(
+                  title: "Billing Details",
+                  icon: Icons.offline_bolt,
+                  onPress: () {}),
+              ProfileMenuWidget(
+                  title: "User Management",
+                  icon: Icons.offline_bolt,
+                  onPress: () {}),
+              const Divider(),
+              const SizedBox(height: 10),
+              ProfileMenuWidget(
+                  title: "Information",
+                  icon: Icons.offline_bolt,
+                  onPress: () {}),
+              ProfileMenuWidget(
+                  title: "Logout",
+                  icon: Icons.offline_bolt,
+                  textColor: Colors.red,
+                  endIcon: false,
+                  onPress: () {
+                    //TODO: mostraa dialogo para confirmar logout
+                  }),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class ProfileMenuWidget extends StatelessWidget {
+  const ProfileMenuWidget({
+    Key? key,
+    required this.title,
+    required this.icon,
+    required this.onPress,
+    this.endIcon = true,
+    this.textColor,
+  }) : super(key: key);
+
+  final String title;
+  final IconData icon;
+  final VoidCallback onPress;
+  final bool endIcon;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+//    var isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
+
+    return ListTile(
+      onTap: onPress,
+      leading: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: colors.primary.withOpacity(0.1),
+        ),
+        child: Icon(icon, color: colors.primary),
+      ),
+      title: Text(title,
+          style:
+              Theme.of(context).textTheme.bodyText1?.apply(color: textColor)),
+      trailing: endIcon
+          ? Container(
+              width: 30,
+              height: 30,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Colors.grey.withOpacity(0.1),
+              ),
+              child: const Icon(Icons.arrow_forward_rounded,
+                  size: 18.0, color: Colors.grey))
+          : null,
     );
   }
 }
