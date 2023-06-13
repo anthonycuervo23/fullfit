a8d44f0a04490e1470b7aa433e90d7a4f908324e
Jean Cuervo <jean.cuervo@datagroup.de>
2023-06-13 12:51:46 +0200

    se cambio las etiquetas de navegacion

--- lib/presentation/widgets/shared/custom_bottom_navbar.dart
+++ lib/presentation/widgets/shared/custom_bottom_navbar.dart
@@ -20,15 +20,15 @@ class CustomBottomNavigationBar extends StatelessWidget {
         currentIndex: navigationShell.currentIndex,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center_rounded),
-            label: 'Workouts',
+            icon: Icon(Icons.home_rounded),
+            label: 'Overview',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.food_bank_rounded),
             label: 'Nutrition',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline_rounded),
+            icon: Icon(Icons.person_rounded),
             label: 'Profile',
           ),
         ]);
